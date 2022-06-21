6b25500ac4548843d4adf8cc7ec4ec2797913a85
hoangtranduyLONG <duylonghindu@gmail.com~>
2022-06-21 10:42:41 +0700

    done

---
+++ src/main/java/com/example/demo/service/ICustomerService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Customer;
+import com.example.demo.service.IGeneralService;
+import com.example.demo.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
